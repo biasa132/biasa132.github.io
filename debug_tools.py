3e80c531ca0bdc97f26c22ae5a3bf850fd476c7e
m29752885-glitch <m29752885@gmail.com>
2026-01-02 10:44:08 +0700

    Create debug_tools.py

---
+++ debug_tools.py
@@ -0,0 +1,12 @@
+# debug_tools.py
+DEBUG_MODE = True  # True = aktifkan debug, False = nonaktifkan
+
+def debug_log(msg):
+    """Cetak pesan debug jika DEBUG_MODE True"""
+    if DEBUG_MODE:
+        print(f"[DEBUG] {msg}")
+
+def cetak(msg):
+    """Cetak biasa"""
+    print(msg)
+  
